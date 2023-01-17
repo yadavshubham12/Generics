24cd66ec158c115d5c1e85cb6056aef88ef1735f
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-17 15:40:37 +0530

    ADDED: Added More than 3 Parameters

--- src/main/java/FindMax.java
+++ src/main/java/FindMax.java
@@ -1,31 +1,7 @@
-public class FindMax<T extends  Comparable<T>> {
-    private T num1, num2, num3;
-
-    public FindMax(T num1, T num2, T num3) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
-    }
-
-    public T testMaximum() {
-        T max = num1;
-        if (num2.compareTo(max) > 0){
-            max = num2;
+import java.util.Arrays;
+public class FindMax {
+    public static <T extends Comparable<T>> T  findMax(T... nums ) {
+        Arrays.sort(nums);
+        return nums[nums.length - 1];
         }
-        if (num3.compareTo(max) > 0){
-            max = num3;
-        }
-        return max;
-    }
-    public static <T extends Comparable<T>> T testMaximum(T num1, T num2, T num3) {
-
-        T max = num1;
-        if (num2.compareTo(max) > 0){
-            max = num2;
-        }
-        if (num3.compareTo(max) > 0){
-            max = num3;
-        }
-        return max;
-    }
 }
--- src/test/java/MaxNumTest.java
+++ src/test/java/MaxNumTest.java
@@ -5,25 +5,17 @@ class MaxNumTest {
     @Test
     public static void main(String[] args) {
 
-        Integer num1 = 15;
-        Integer num2 = 10;
-        Integer num3 = 7;
         Integer expected = 15;
-        Integer result = FindMax.testMaximum(num1, num2, num3);
+        Integer result = FindMax.findMax(15, 10, 7, 12, 5, 18);
         assert result.equals (expected);
 
-        float num4 = 10.0f;
-        float num5 = 15.0f;
-        float num6= 7.0f;
+
         float expected2 = 15.0f;
-        float result2 = FindMax.testMaximum(num1, num2, num3);
+        float result2 = FindMax.findMax(15.0f, 10.0f, 7.0f, 12.0f, 5.0f, 18.0f);
         assert result2 == (expected2);
 
-        String str1 = "Apple";
-        String str2 = "Peach";
-        String str3 = "Banana";
-        String expected3 = "Apple";
-        String result3 = FindMax.testMaximum(str1, str2, str3);
+        String expected3 = "Orange";
+        String result3 = FindMax.findMax("Apple", "Peach", "Banana", "Orange", "Grape");
         assert result3.equals (expected3);
     }
 }
