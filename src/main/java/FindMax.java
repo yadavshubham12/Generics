203c7bd198e6280eea9407f2e8589e19c250a3f8
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-17 13:21:31 +0530

    ADDED: print to create a generics class take 3 variables

--- src/main/java/FindMax.java
+++ src/main/java/FindMax.java
@@ -1,6 +1,24 @@
-public class FindMax {
+public class FindMax<T extends  Comparable<T>> {
+    private T num1, num2, num3;
 
-    public static <T extends Comparable<T>> T findMax(T num1, T num2, T num3){
+    public FindMax(T num1, T num2, T num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+
+    public T testMaximum() {
+        T max = num1;
+        if (num2.compareTo(max) > 0){
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0){
+            max = num3;
+        }
+        return max;
+    }
+    public static <T extends Comparable<T>> T testMaximum(T num1, T num2, T num3) {
+
         T max = num1;
         if (num2.compareTo(max) > 0){
             max = num2;
--- src/test/java/MaxNumTest.java
+++ src/test/java/MaxNumTest.java
@@ -9,21 +9,21 @@ class MaxNumTest {
         Integer num2 = 10;
         Integer num3 = 7;
         Integer expected = 15;
-        Integer result = FindMax.findMax(num1, num2, num3);
+        Integer result = FindMax.testMaximum(num1, num2, num3);
         assert result.equals (expected);
 
         float num4 = 10.0f;
         float num5 = 15.0f;
         float num6= 7.0f;
         float expected2 = 15.0f;
-        float result2 = FindMax.findMax(num1, num2, num3);
+        float result2 = FindMax.testMaximum(num1, num2, num3);
         assert result2 == (expected2);
 
         String str1 = "Apple";
         String str2 = "Peach";
         String str3 = "Banana";
         String expected3 = "Apple";
-        String result3 = FindMax.findMax(str1, str2, str3);
+        String result3 = FindMax.testMaximum(str1, str2, str3);
         assert result3.equals (expected3);
     }
 }
