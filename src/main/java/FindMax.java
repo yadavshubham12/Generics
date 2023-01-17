e49bb6aaba2ebcd2f723de798d277a795438cc4d
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-17 13:03:35 +0530

    ADDED: print to all 3 is added in one generics method

--- src/main/java/FindMax.java
+++ src/main/java/FindMax.java
@@ -1,12 +1,12 @@
 public class FindMax {
 
-    public static String findMax(String str1, String str2, String str3){
-        String max = str1;
-        if (str2.compareTo(max) > 0){
-            max = str2;
+    public static <T extends Comparable<T>> T findMax(T num1, T num2, T num3){
+        T max = num1;
+        if (num2.compareTo(max) > 0){
+            max = num2;
         }
-        if (str3.compareTo(max) > 0){
-            max = str3;
+        if (num3.compareTo(max) > 0){
+            max = num3;
         }
         return max;
     }
--- src/test/java/MaxNumTest.java
+++ src/test/java/MaxNumTest.java
@@ -5,25 +5,25 @@ class MaxNumTest {
     @Test
     public static void main(String[] args) {
 
+        Integer num1 = 15;
+        Integer num2 = 10;
+        Integer num3 = 7;
+        Integer expected = 15;
+        Integer result = FindMax.findMax(num1, num2, num3);
+        assert result.equals (expected);
+
+        float num4 = 10.0f;
+        float num5 = 15.0f;
+        float num6= 7.0f;
+        float expected2 = 15.0f;
+        float result2 = FindMax.findMax(num1, num2, num3);
+        assert result2 == (expected2);
+
         String str1 = "Apple";
         String str2 = "Peach";
         String str3 = "Banana";
-        String expected = "Apple";
-        String result = FindMax.findMax(str1, str2, str3);
-        assert result.equals (expected);
-
-        str1 = "Peach";
-        str2 = "Apple";
-        str3 = "Banana";
-        expected = "Apple";
-        result = FindMax.findMax(str1, str2, str3);
-        assert result.equals (expected);
-
-        str1 = "Peach";
-        str2 = "Banana";
-        str3 = "Apple";
-        expected = "Apple";
-        result = FindMax.findMax(str1, str2, str3);
-        assert result.equals (expected);
+        String expected3 = "Apple";
+        String result3 = FindMax.findMax(str1, str2, str3);
+        assert result3.equals (expected3);
     }
 }
